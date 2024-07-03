8c45e2e8df9ce693d9914547bd94780fe8445dbb
corosualidaquila <c.sualidaquila@gmail.com>
2024-07-03 10:20:29 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,23 +1,3 @@
-$(function(){
-	var welcomeSection = $('.welcome-section'),
-		enterButton = welcomeSection.find('.enter-button');
-
-		setTimeout(function(){
-		welcomeSection.removeClass('content-hidden');
-		}, 800);
-
-	enterButton.on('click', function(e){
-		e.preventDefault();
-    	welcomeSection.addClass('content-hidden').fadeOut();
-
-});
-
-});
-
-
-
-
-
 
 function myFunction() {
    var element = document.body;
